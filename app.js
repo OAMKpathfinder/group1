74291a887f627dc51e6de344c50fe73fb99b0772
t7haki01 <t7haki01@students.oamk.fi>
2019-11-12 09:38:34 +0200

    dotenv dependency added, basic routes and models added

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ var express = require('express');
 var app = express();
 const path = require('path');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 /**
  * For the cors policy, lazy implementation which is not including header
  * separately, using one dependency "cors"
  * 
+ * Which means that if we wanted to use API test in different url,
+ * we need to allow cors policy by uncommenting it
+ * 
  * Remember to get rid of this in production
  */
 // app.use(cors());
 
-app.use(express.static(__dirname + '/assets'));
+var homePropertiesRouter = require('./routes/homeProperties');
+app.use('/homeProperties', homePropertiesRouter);
+
+var outerWallRouter = require('./routes/outerWall');
+app.use('/outerWall', outerWallRouter);
+
+
 app.use(express.static(path.join(__dirname, 'dist/PathFinder')));
 
 app.get('*', (req, res) => {
@@ -22,8 +34,8 @@ app.get('*', (req, res) => {
 
 //Port might be specified here directly or separately from external env file setting etc
 var port = 3000;
+require('dotenv').config()
 
-
-app.listen(port, function() {
-    console.log('Server listening on port: ' + port);
-});
+app.listen( process.env.SERVER_PORT || port, () =>
+  console.log('server running on localhost:3000')
+);
