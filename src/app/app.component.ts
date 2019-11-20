78511f15fcdaf6fdef322675d1ba12873270c946
t7haki01 <t7haki01@students.oamk.fi>
2019-11-20 10:06:16 +0200

    comment added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,8 @@ export class AppComponent {
   
   lorem ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
+  //breadcrumb required 2 Inputs, id for referring the div "height" (from top and next div top?)
+  //phase for used to name the breadcrumb and referring the li to assign/remove active class
   ids = ["el1","el2","el3"];
   phases = ["phase1","phase2","phase3"];
 }
