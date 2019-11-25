5c6c0bf7a4f6e389dc095213f5e078518510ce69
t7haki01 <t7haki01@students.oamk.fi>
2019-11-25 13:05:47 +0200

    active status & scoll movement added

--- src/app/breadcrumb/breadcrumb.component.ts
+++ src/app/breadcrumb/breadcrumb.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
-  styleUrls: ['./breadcrumb.component.css']
+  styleUrls: ['./breadcrumb.component.css'],
 })
 export class BreadcrumbComponent implements OnInit {
   
@@ -11,13 +12,25 @@ export class BreadcrumbComponent implements OnInit {
   @Input() phases: string[] = [];
 
   properties: any = [];
+  private currentDiv: string = null;
+  private extra: number = 30;
 
   constructor() { }
 
   scrollTo(id:string): void{
     if(document.getElementById(id)){
+      this.activeClassOnly(id+"-class")
       document.getElementById(id).scrollIntoView({ block: 'start',  behavior: 'smooth' });
     }
+  }
+
+  activeClassOnly(id: string): void{
+    for(let i = 0; i < this.ids.length; i++){
+      if(document.getElementById(this.ids[i]+"-class").classList.contains("active-bc")){
+        document.getElementById(this.ids[i]+"-class").classList.remove("active-bc")
+      }
+    }
+    document.getElementById(id).classList.add("active-bc")
   }
 
   ngOnInit() {
@@ -30,44 +43,57 @@ export class BreadcrumbComponent implements OnInit {
       });
     }
 
+    window.addEventListener("load", e => {
+      this.scrollToActive();
+      this.activeClassOnly(this.currentDiv+"-id-class");
+    });
+    
     window.addEventListener("scroll", event =>{
-      let current = window.pageYOffset;
-      let total = document.body.scrollHeight;
+      this.scrollToActive();
+      this.activeClassOnly(this.currentDiv+"-id-class");
+    });
+    
+  }
+  
+  scrollToActive(): void{
+    let current = window.pageYOffset;
+    let total = document.body.scrollHeight;
+    for(let j = 0; j<this.ids.length;j++){
+      // console.log(document.getElementById(this.ids[j]).offsetTop);
 
-      for(let j = 0; j<this.ids.length;j++){
-        if( j == (this.ids.length-1) ){
-          let top = document.getElementById(this.ids[j]).offsetTop;
-          let nextTop = total
-          let height = nextTop - top;
-          if(current >= top && current <= (top + height ) ){
-            for(let k = 0; k<this.phases.length;k++){
-              if(j==k){
-                document.getElementById(this.phases[k]).classList.add("active");
-              }
-              else{
-                document.getElementById(this.phases[k]).classList.remove("active");
-              }
+      if( j == (this.ids.length-1) ){
+        let top = document.getElementById(this.ids[j]).offsetTop;
+        let nextTop = total
+        let height = nextTop - top;
+        if(current >= top-this.extra && current <= (top + height ) ){
+          for(let k = 0; k<this.phases.length;k++){
+            if(j==k){
+              document.getElementById(this.phases[k]).classList.add("active");
+              this.currentDiv = this.phases[k];
+            }
+            else{
+              document.getElementById(this.phases[k]).classList.remove("active");
             }
           }
         }
-        else{
-          let top = document.getElementById(this.ids[j]).offsetTop;
-          let nextTop = document.getElementById(this.ids[j+1]).offsetTop;
-          let height = nextTop - top;
-          if(current >= top && current <= (top + height ) ){
-            for(let k = 0; k<this.phases.length;k++){
-              if(j==k){
-                document.getElementById(this.phases[k]).classList.add("active");
-              }
-              else{
-                document.getElementById(this.phases[k]).classList.remove("active");
-              }
-            }
-          }   
-        }
       }
-    });
-
+      else{
+        let top = document.getElementById(this.ids[j]).offsetTop;
+        let nextTop = document.getElementById(this.ids[j+1]).offsetTop;
+        let height = nextTop - top;
+        if(current >= top-this.extra && current <= (top + height ) ){
+          for(let k = 0; k<this.phases.length;k++){
+            if(j==k){
+              document.getElementById(this.phases[k]).classList.add("active");
+              this.currentDiv = this.phases[k];
+            }
+            else{
+              document.getElementById(this.phases[k]).classList.remove("active");
+            }
+          }
+        }   
+      }
+    }
   }
 
 }
