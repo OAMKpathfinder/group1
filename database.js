d109e551e5f3affc6d90cbe57c43e599a0f93f0f
t7haki01 <t7haki01@students.oamk.fi>
2019-11-25 09:50:06 +0200

    breadscrum css width modified

--- database.js
+++ database.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const Pool = require('pg').Pool
 
 const connection = new Pool({
-    user: "postgres",
-    host:  "localhost",
-    database: "DummyPF",
-    password: "wasd",
-    port: "5432",
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
 });
 
 module.exports = connection;
