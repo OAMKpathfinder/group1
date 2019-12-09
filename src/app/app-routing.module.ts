122a197c43dc82d713fd94fa2ae7dfc07281f6fc
t7haki01 <t7haki01@students.oamk.fi>
2019-12-09 13:19:46 +0200

    compare error handling added, chart layout modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
-<<<<<<< HEAD
 import { ResultWholePrivateComponent } from './result-whole-private/result-whole-private.component';
-
-const routes: Routes = [
-  {path: 'mainpage', component: MainpageComponent},
-  {path: 'landing', component: LandingComponent},
-  {path: 'private_page', component: ResultWholePrivateComponent},
-  {path: '', redirectTo: '/landing', pathMatch: 'full'},
-=======
-import { ResultTableComponent } from './result-table/result-table.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 const routes: Routes = [
   {path: 'mainpage', component: MainpageComponent},
+  {path: 'admin', component: AdminPanelComponent},
+  {path: 'private_page', component: ResultWholePrivateComponent},
   {path: '', component: LandingComponent},
-  {path: 'results', component: ResultTableComponent},
-  {path: 'admin', component: AdminPanelComponent}
->>>>>>> master
 ];
 
 @NgModule({
--- src/app/chart-modal/chart-modal.component.ts
+++ src/app/chart-modal/chart-modal.component.ts
@@ -21,7 +21,7 @@ export class ChartModalComponent implements OnInit {
       }
     },
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = [];
   public barChartType: ChartType = 'horizontalBar';
   public barChartLegend = true;
   public barChartPlugins;
@@ -35,19 +35,36 @@ export class ChartModalComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any,){
       this.data = data;
   }
+  ngOnInit(){
+    this.initData()
+  }
   
-  ngOnInit() {
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'A', yAxisID: 'A' },
-    { data: [-28, -48, -40, -19, -86, -27, -90], label: 'B', yAxisID: 'A' }
-    for(let i = 0; i<this.data.length; i++){
+  initData() {
+    for(let i = 0; i<this.data.data.length; i++){
+      let solution = [];
+      for(let j = 0; j<this.data.data[i].suggestion.length; j++){
+        if(i == 0){
+          solution.push(
+            this.data.data[i].suggestion[j].percent,
+            (this.data.data[i].suggestion[j].cost/1000) )
+        }
+        else{
+          solution.push(
+            -this.data.data[i].suggestion[j].percent,
+            -(this.data.data[i].suggestion[j].cost/1000) )
+        }
+      }
       this.barChartData.push(
         {
-          label: this.data[i].property,
-          data: [
-            this.data[i].suggestion[0]
-          ]
+          label: this.data.data[i].property,
+          yAxisID: "A",
+          data: solution
         }
-      )      
+      )
+      this.barChartLabels.push(
+        'Suggest '+ (i+1) + ' Estimated Improvement (%)',
+        'Suggest '+ (i+1) + ' Estimated Cost (K/€)'
+      )
     }
   }
   onCancel(): void {
@@ -68,17 +85,17 @@ export class ChartModalComponent implements OnInit {
       console.log(event, active);
     }
   
-    public randomize(): void {
+    public randomize(index:number): void {
       // Only Change 3 values
-      const data = [
-        Math.round(Math.random() * 100),
-        59,
-        80,
-        (Math.random() * 100),
-        56,
-        (Math.random() * 100),
-        40];
-      this.barChartData[0].data = data;
+      // const data = [
+      //   Math.round(Math.random() * 100),
+      //   59,
+      //   80,
+      //   (Math.random() * 100),
+      //   56,
+      //   (Math.random() * 100),
+      //   40];
+      // this.barChartData[0].data = data;
     }
 
 }
