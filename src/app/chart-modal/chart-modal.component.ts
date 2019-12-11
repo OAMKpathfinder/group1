116fd3686adaa5b7475c68e9a84f83f5949f8651
t7haki01 <t7haki01@students.oamk.fi>
2019-12-11 15:17:49 +0200

    chart suggestion update interactive per property added

--- src/app/chart-modal/chart-modal.component.ts
+++ src/app/chart-modal/chart-modal.component.ts
@@ -29,6 +29,7 @@ export class ChartModalComponent implements OnInit {
   //Push to barChartData, used for modifying the chart as interactive
   //Designed to push in order, total E %, total cost, -> suggest E, suggest cost
   public barChartData: ChartDataSets[] = [];
+  private originalDataSet: any[] = [];
 
   constructor(
     private APIService: APIService,
@@ -37,6 +38,7 @@ export class ChartModalComponent implements OnInit {
       this.data = data;
   }
   ngOnInit(){
+    console.log(this.data.data)
     this.initData();
   }
   
@@ -76,9 +78,19 @@ export class ChartModalComponent implements OnInit {
       this.barChartLabels.push(labelToPush[j]);    
     }
     this.barChartData[0].data.push(40,0)
+    dataToPush[0].data.push(40,0);
     this.barChartData[1].data.push(35,0)
+    dataToPush[1].data.push(35,0);
 
-    this.barChartLabels.push('Total E %', 'Total E cost (k/€)');
+    for(let i in dataToPush){
+      let dataSet = [];
+      for(let j in dataToPush[i].data){
+        dataSet.push(dataToPush[i].data[j])
+      }
+      this.originalDataSet.push(dataSet);
+    }
+
+    this.barChartLabels.push('Total E (%)', 'Total E cost (k/€)');
   }
   onCancel(): void {
     console.log("Cancel clicked");
@@ -97,35 +109,57 @@ export class ChartModalComponent implements OnInit {
     public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
       console.log(event, active);
     }
-  
-    public affect(index:number, ): void {
-      let efficiency = 0;
-      let cost = 1;
-      if(index){
-        cost = 2 * (index - 1) + 1 ;
-        efficiency = cost + 1  ;
-      }
-      let dataSet = this.barChartData;
-      for(let i in dataSet){
-        let affectedPercent: number = ( <number> (dataSet[i].data[(dataSet[i].data.length - 2)]) ) - ( <number> (dataSet[i].data[efficiency]) );
-        let affectedCost: number = (<number> (dataSet[i].data[(dataSet[i].data.length - 1)]) ) + ( <number> (dataSet[i].data[cost]) );
 
-        const dataTem: number[] = [
-          (<number> dataSet[i].data[0]),
-          (<number>dataSet[i].data[1]),
-          (<number>dataSet[i].data[2]),
-          (<number>dataSet[i].data[3]),
-          affectedPercent >= 0 ? affectedPercent: 0,
-          affectedCost,
-        ]
-        this.barChartData[i].data = dataTem;
+    /**
+     * 
+     * @param - index for dataset of suggestions, isA for dataset of target
+     * 
+     */
+  
+    public affect(index:number, isA:boolean): void {
+
+      let cost = 2 * (index - 1) + 3 ;
+      let efficiency = cost - 1  ;
+
+      let dataSet = this.barChartData;
+      for(let i = 0; i<dataSet.length; i++){
+        if(isA && i == 0){
+          let affectedPercent: number = ( <number> (dataSet[i].data[(dataSet[i].data.length - 2)]) ) - ( <number> (dataSet[i].data[efficiency]) );
+          let affectedCost: number = (<number> (dataSet[i].data[(dataSet[i].data.length - 1)]) ) + ( <number> (dataSet[i].data[cost]) );
+
+          const dataTem: number[] = [
+            efficiency == 0 ? 0 : (<number> dataSet[i].data[0]) ,
+            cost == 1 ? 0 : (<number> dataSet[i].data[1]) ,
+            efficiency == 2 ? 0 : (<number> dataSet[i].data[2]) ,
+            cost == 3 ? 0 : (<number> dataSet[i].data[3]) ,
+            affectedPercent >= 0 ? affectedPercent: 0,
+            affectedCost,
+          ]
+          this.barChartData[i].data = dataTem;
+        }
+        else if(!isA && i == 1){
+          let affectedPercent: number = ( <number> (dataSet[i].data[(dataSet[i].data.length - 2)]) ) - ( <number> (dataSet[i].data[efficiency]) );
+          let affectedCost: number = (<number> (dataSet[i].data[(dataSet[i].data.length - 1)]) ) + ( <number> (dataSet[i].data[cost]) );
+  
+          const dataTem: number[] = [
+            efficiency == 0 ? 0: (<number> dataSet[i].data[0]),
+            cost == 1 ? 0: (<number> dataSet[i].data[1]),
+            efficiency == 2 ? 0: (<number> dataSet[i].data[2]),
+            cost == 3 ? 0: (<number> dataSet[i].data[3]),
+            affectedPercent >= 0 ? affectedPercent: 0,
+            affectedCost,
+          ]
+          this.barChartData[i].data = dataTem;
+        }
       }
     }
 
     public reset(): void{
-      this.barChartData.length = 0;
-      this.barChartLabels.length = 0;
-      this.initData();
+      this.barChartData[0].data.length = 0;
+      this.barChartData[0].data = this.originalDataSet[0] ;
+
+      this.barChartData[1].data.length = 0;
+      this.barChartData[1].data = this.originalDataSet[0] ;
     }
 
 }
